2c568ac686264611d58c6c6d12a34d617ff2f193
Robert Russo <rrusso44@gmail.com>
2017-03-08 15:01:06 -0500

    Doesn't Break!

--- MobileClockIn/MobileClockIn/ViewController.designer.cs
+++ MobileClockIn/MobileClockIn/ViewController.designer.cs
@@ -13,14 +13,6 @@ namespace MobileClockIn
     [Register ("ViewController")]
     partial class ViewController
     {
-        [Action ("UIButton34_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void UIButton34_TouchUpInside (UIKit.UIButton sender);
-
-        [Action ("UIButton6_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void UIButton6_TouchUpInside (UIKit.UIButton sender);
-
         void ReleaseDesignerOutlets ()
         {
         }
