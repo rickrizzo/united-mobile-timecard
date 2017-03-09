c57b4c66af8d7abd4cb4ff7eb8e67ea0981a585b
David Khachaturyan <khachd@rpi.edu>
2017-03-09 18:19:41 -0500

    secondary commit

---
+++ MobileClockIn/ua-server/src/ua/base/BaseDAO.java
@@ -0,0 +1,11 @@
+package ua.base;
+
+public abstract class BaseDAO {
+
+	public static void main(String[] args) {
+		System.out.println("This was a triumph.");
+		System.out.println("I'm making a note here - huge success!");
+		System.out.println("It's hard to overstate my satisfaction.");
+	}
+
+}
