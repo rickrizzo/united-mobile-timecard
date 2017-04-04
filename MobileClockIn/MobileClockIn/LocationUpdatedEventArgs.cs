9476c8d072f9b54b4f899683f6f78c50f64bb25b
Robert Russo <rrusso44@gmail.com>
2017-04-04 00:53:39 -0400

    Event Arguments

---
+++ MobileClockIn/MobileClockIn/LocationUpdatedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.Contracts;
+using CoreLocation;
+
+namespace MobileClockIn
+{
+	public class LocationUpdatedEventArgs
+	{
+		CLLocation cLLocation;
+
+		public LocationUpdatedEventArgs(CLLocation cLLocation)
+		{
+			this.cLLocation = cLLocation;
+		}
+
+		public CLLocation Location
+		{
+			get { return cLLocation; }
+		}
+	}
+}
